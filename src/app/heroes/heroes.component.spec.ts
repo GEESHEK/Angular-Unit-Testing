721374bceef906ede3c630316e1c1485579b384f
Gee Chong Shek <geeshek95@outlook.com>
2025-01-08 17:04:49 +0000

    Testing interactions (check if a method has been called)

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -8,27 +8,39 @@ describe('HeroesComponent', () => {
 
   beforeEach(() => {
     HEROES = [
-      {id:1, name: 'h1', strenght: 8},
-      {id:2, name: 'h2', strenght: 24},
-      {id:3, name: 'h3', strenght: 55}
+      {id: 1, name: 'h1', strenght: 8},
+      {id: 2, name: 'h2', strenght: 24},
+      {id: 3, name: 'h3', strenght: 55}
     ]
 
     mockHeroesService = jasmine.createSpyObj('HeroesService', ['getHeroes', 'addHero', 'deleteHero']);
 
     component = new HeroesComponent(mockHeroesService);
+  })
 
-    describe('delete', () => {
+  describe('delete', () => {
 
-      it('should remove the indicated hero from the heroes list', () => {
-        //just returning an observable from the subscribe method
-        mockHeroesService.deleteHeroes.and.returnValue(of(true));
+    // State based test
+    it('should remove the indicated hero from the heroes list', () => {
+      //just returning an observable from the subscribe method
+      mockHeroesService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
 
-        component.heroes = HEROES;
+      component.delete(HEROES[2]);
 
-        component.delete(HEROES[2]);
+      expect(component.heroes.length).toBe(2);
+    })
 
-        expect(component.heroes.length).toBe(2);
-      });
+
+    // Testing interactions
+    it('should call deleteHero', () => {
+      mockHeroesService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroesService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
     })
   })
 })
+
