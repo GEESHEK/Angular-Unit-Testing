bfb0f34991c3ea352f60068c1e5aeeab803ddf16
Gee Chong Shek <geeshek95@outlook.com>
2025-01-08 16:40:48 +0000

    Mocking to isolate code

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,34 @@
+﻿import {HeroesComponent} from "./heroes.component";
+import {of} from "rxjs";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroesService;
+
+  beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'h1', strenght: 8},
+      {id:2, name: 'h2', strenght: 24},
+      {id:3, name: 'h3', strenght: 55}
+    ]
+
+    mockHeroesService = jasmine.createSpyObj('HeroesService', ['getHeroes', 'addHero', 'deleteHero']);
+
+    component = new HeroesComponent(mockHeroesService);
+
+    describe('delete', () => {
+
+      it('should remove the indicated hero from the heroes list', () => {
+        //just returning an observable from the subscribe method
+        mockHeroesService.deleteHeroes.and.returnValue(of(true));
+
+        component.heroes = HEROES;
+
+        component.delete(HEROES[2]);
+
+        expect(component.heroes.length).toBe(2);
+      });
+    })
+  })
+})
