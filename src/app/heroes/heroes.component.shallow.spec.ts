0c1306b1dbe297c587ac684a26d134ea85820c89
Gee Chong Shek <geeshek95@outlook.com>
2025-02-08 13:37:51 +0000

    Mocking child components

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,16 +1,26 @@
 ﻿import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {HeroesComponent} from "./heroes.component";
 import {HeroService} from "../hero.service";
-import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output} from "@angular/core";
 import {of} from "rxjs";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {MessageService} from "../message.service";
+import {Hero} from "../hero";
 
 describe('HeroesComponent (shallow tests', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
-  // let mockMessageService;
   let HEROES;
+
+  //creating a mock child instead of ignoring the template errors with NO_ERRORS_SCHEMA
+  @Component({
+    selector: "app-hero",
+    template: "<div></div>",
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    @Output() delete = new EventEmitter();
+  }
 
   beforeEach(() => {
     HEROES = [
@@ -19,18 +29,19 @@ describe('HeroesComponent (shallow tests', () => {
       {id: 3, name: 'h3', strength: 55}
     ]
     mockHeroService = jasmine.createSpyObj(["getHeroes", 'addHeroes', 'deleteHeroes']);
-    // mockMessageService = jasmine.createSpyObj(["add", "clear"]);
 
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [
+        HeroesComponent,
+        FakeHeroComponent,
+      ],
       //provide services for injection but we should mock
       providers: [
         { provide: HeroService, useValue: mockHeroService },
-        // { provide: MessageService, useClass: mockMessageService }
       ],
       imports: [HttpClientTestingModule],
-      // used to ignore the child component
-      schemas: [NO_ERRORS_SCHEMA]
+      // used to ignore the child component > errors in templates ignored
+      // schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(HeroesComponent);
   });
