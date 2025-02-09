9633e0d0a03c9500209c40883dda73f743e00ad7
Gee Chong Shek <geeshek95@outlook.com>
2025-02-09 15:20:31 +0000

    using fakeAsync to treat async, synchronous

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-﻿import {ComponentFixture, TestBed} from "@angular/core/testing";
+﻿import {ComponentFixture, fakeAsync, flush, TestBed} from "@angular/core/testing";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {ActivatedRoute} from "@angular/router";
 import {HeroService} from "../hero.service";
@@ -7,40 +7,52 @@ import {of} from "rxjs";
 import {FormsModule} from "@angular/forms";
 
 describe('HeroDetailComponent', () => {
-  let fixture: ComponentFixture<HeroDetailComponent>;
-  let mockActivatedRoute, mockHeroService, mockLocation;
+    let fixture: ComponentFixture<HeroDetailComponent>;
+    let mockActivatedRoute, mockHeroService, mockLocation;
 
-  beforeEach(() => {
-    mockActivatedRoute = {
-      snapshot: {
-        paramMap: {
-          get: () => {
-            return '3';
-          }
+    beforeEach(() => {
+        mockActivatedRoute = {
+            snapshot: {
+                paramMap: {
+                    get: () => {
+                        return '3';
+                    }
+                }
+            }
         }
-      }
-    }
 
-    mockHeroService = jasmine.createSpyObj(["getHero", "updateHero"]);
-    mockLocation = jasmine.createSpyObj(["back"]);
+        mockHeroService = jasmine.createSpyObj(["getHero", "updateHero"]);
+        mockLocation = jasmine.createSpyObj(["back"]);
 
-    TestBed.configureTestingModule({
-      declarations: [HeroDetailComponent],
-      imports: [FormsModule],
-      providers: [
-        {provide: ActivatedRoute, useValue: mockActivatedRoute},
-        {provide: HeroService, useValue: mockHeroService},
-        {provide: Location, useValue: mockLocation},
-      ]
+        TestBed.configureTestingModule({
+            declarations: [HeroDetailComponent],
+            imports: [FormsModule],
+            providers: [
+                {provide: ActivatedRoute, useValue: mockActivatedRoute},
+                {provide: HeroService, useValue: mockHeroService},
+                {provide: Location, useValue: mockLocation},
+            ]
+        });
+        fixture = TestBed.createComponent(HeroDetailComponent);
+
+        mockHeroService.getHero.and.returnValue(of({id: 3, name: "h3", strength: 100}));
     });
-    fixture = TestBed.createComponent(HeroDetailComponent);
 
-    mockHeroService.getHero.and.returnValue(of({id: 3, name: "h3", strength: 100}));
-  });
+    it('should render hero name in a h2 tag', () => {
+        fixture.detectChanges();
 
-  it('should render hero name in a h2 tag', () => {
-    fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('h2').textContent).toContain('H3');
+    });
 
-    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('H3');
-  })
+    //async test, wrap call back in fakeAsync
+    //treat async as synchronous
+    it('should call updateHero when save is called', fakeAsync(() => {
+        mockHeroService.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+        flush();
+
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+    }))
 })
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -33,33 +33,33 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
-  save(): void {
-    var p = new Promise((resolve) => {
-      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
-      resolve();
-    });
-  }
-
   // save(): void {
-  //   debounce(() => {
-  //     this.heroService.updateHero(this.hero)
-  //       .subscribe(() => this.goBack());
-  //   }, 250, false)();
+  //   var p = new Promise((resolve) => {
+  //     this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  //     resolve();
+  //   });
   // }
+
+  save(): void {
+    debounce(() => {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }, 250, false)();
+  }
 }
 
-// function debounce(func, wait, immediate) {
-//   var timeout;
-//   return function () {
-//     var context = this,
-//       args = arguments;
-//     var later = function () {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     var callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// }
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
